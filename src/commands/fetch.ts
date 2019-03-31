d706c77d2212fac7ec2aeefd1809c508adb84ce2
MatejTuray <matej.turay@gmail.com>
2019-03-31 13:24:14 +0200

    examplex

--- src/commands/fetch.ts
+++ src/commands/fetch.ts
@@ -1,120 +1,119 @@
-import { flags, Command } from "@oclif/command";
-const color = require("colors-cli");
-const getTwitchLink = require("node-twitch-link");
-let ffmpeg = require("fluent-ffmpeg");
-import cli from "cli-ux";
-const path = require("path");
-const _cliProgress = require("cli-progress");
+import { flags, Command } from '@oclif/command'
+const color = require('colors-cli')
+const getTwitchLink = require('node-twitch-link')
+let ffmpeg = require('fluent-ffmpeg')
+import cli from 'cli-ux'
+const path = require('path')
+const _cliProgress = require('cli-progress')
 export default class Fetch extends Command {
-  static description = "Downloads and processes Twitch.tv video";
+  static description = 'Downloads and processes Twitch.tv video'
 
   static examples = [
     `$ twitchvod fetch https://www.twitch.tv/videos/401113393 --res=720p60 --out=Some path for output file here
 `
-  ];
+  ]
 
   static flags = {
-    help: flags.help({ char: "h" }),
+    help: flags.help({ char: 'h' }),
     res: flags.string({
-      char: "r",
-      description: "Video resolution",
-      default: "chunked",
+      char: 'r',
+      description: 'Video resolution',
+      default: 'chunked',
       options: [
-        "chunked",
-        "720p60",
-        "720p30",
-        "480p30",
-        "360p30",
-        "160p30",
-        "audio_only"
+        'chunked',
+        '720p60',
+        '720p30',
+        '480p30',
+        '360p30',
+        '160p30',
+        'audio_only'
       ]
     }),
     out: flags.string({
-      char: "o",
-      description: "Output file path",
-      default: ""
+      char: 'o',
+      description: 'Output file path',
+      default: ''
     })
-  };
+  }
 
   static args = [
     {
-      name: "vod",
+      name: 'vod',
       required: true,
-      description: "Link to twitch VOD url or ID"
+      description: 'Link to twitch VOD url or ID'
     }
-  ];
+  ]
 
   async run() {
-    const warn = color.yellow;
-    const green = color.green;
-    const red = color.red;
-    const { args, flags } = this.parse(Fetch);
+    const warn = color.yellow
+    const green = color.green
+    const red = color.red
+    const { args, flags } = this.parse(Fetch)
     const tokenObj = {
-      client_id: "bc0jezu5plohhw08pud3z5bpojodr9"
-    };
-    const headers = { "Client-ID": tokenObj.client_id };
+      client_id: 'bc0jezu5plohhw08pud3z5bpojodr9'
+    }
     if (args.vod && flags.res) {
-      const twitchUrl = args.vod;
-      let percent = "0";
+      const twitchUrl = args.vod
+      let percent = '0'
       //axios.get();
-      cli.action.start("Fetching video links", `running`, {
+      cli.action.start('Fetching video links', `running`, {
         stdout: true
-      });
+      })
       const link = `${
-        twitchUrl.indexOf("/") === -1
+        twitchUrl.indexOf('/') === -1
           ? `https://twitch.tv/videos/${twitchUrl}`
           : twitchUrl
-      }`;
-      cli.action.stop(green("done!"));
-      console.log(warn("Converting video..."));
+      }`
+      cli.action.stop(green('done!'))
+      console.log(warn('Converting video...'))
       const output =
-        twitchUrl.indexOf("/") === -1
+        twitchUrl.indexOf('/') === -1
           ? twitchUrl
-          : twitchUrl.substr(twitchUrl.lastIndexOf("/") + 1) + ".mp4";
+          : twitchUrl.substr(twitchUrl.lastIndexOf('/') + 1) + '.mp4'
       getTwitchLink(link, tokenObj)
         .then((linkArray: any) => {
           const bar = new _cliProgress.Bar(
             { stream: process.stdout },
             _cliProgress.Presets.shades_classic
-          );
+          )
           const video = linkArray.find(
             (link: { type: string | undefined }) => link.type === flags.res
-          );
+          )
           if (!video) {
             const resolutionArray: any[] = linkArray.map(
               (item: { type: any }) => item.type
-            );
+            )
             console.log(
               warn(
-                "Provided resolution not found, try running --res=option with these options"
+                'Provided resolution not found, try running --res=option with these options'
               ),
               warn(resolutionArray)
-            );
+            )
           }
           ffmpeg(video.url)
-            .on("error", (error: string | undefined) => {
-              console.log(error);
+            .on('error', (error: string | undefined) => {
+              console.log(error)
             })
-            .on("end", () => {
-              bar.stop();
-              console.log(green("Video converted"));
+            .on('end', () => {
+              bar.stop()
+              console.log(green('Video converted'))
             })
-            .on("start", () => {
-              bar.start(100, 0);
-              console.log(video.url);
+            .on('start', () => {
+              bar.start(100, 0)
+              console.log(video.url)
             })
-            .on("progress", (progress: { percent: number }) => {
-              percent = progress.percent.toPrecision(4).toString();
-              7;
-              bar.update(percent);
+            .on('progress', (progress: { percent: number }) => {
+              percent = progress.percent.toPrecision(4).toString()
+              7
+              bar.update(percent)
             })
-            .outputOptions(["-c copy", "-bsf:a aac_adtstoasc", "-f mp4"])
-            .save(flags.out + output);
+            .outputOptions(['-c copy', '-bsf:a aac_adtstoasc', '-f mp4'])
+            .save(flags.out + output)
         })
-        .catch((e: Error) => this.log(red(e.toString())));
+        .catch((e: Error) => this.log(red(e.toString())))
     } else {
-      console.log(red("Error: No url or video id provided"));
-      console.log(warn("hint: run the command with --help flag to learn more"));
+      console.log(red('Error: No url or video id provided'))
+      console.log(warn('hint: run the command with --help flag to learn more'))
     }
   }
 }
--- src/commands/info.ts
+++ src/commands/info.ts
@@ -1,11 +1,11 @@
-import { Command, flags } from "@oclif/command";
-import axios from "axios";
-const moment = require("moment");
-const color = require("colors-cli");
-import cli from "cli-ux";
+import { Command, flags } from '@oclif/command'
+import axios from 'axios'
+const moment = require('moment')
+const color = require('colors-cli')
+import cli from 'cli-ux'
 
 export default class Info extends Command {
-  static description = "Fetches basic information about Twitch.tv video";
+  static description = 'Fetches basic information about Twitch.tv video'
 
   static examples = [
     `$ twitchvod info https://www.twitch.tv/videos/401113393
@@ -21,76 +21,76 @@ export default class Info extends Command {
     Language: en
     Resolutions: 160p30,360p30,480p30,720p30,720p60,chunked
 `
-  ];
+  ]
 
   static flags = {
-    help: flags.help({ char: "h" })
-  };
+    help: flags.help({ char: 'h' })
+  }
 
-  static args = [{ name: "vod" }];
+  static args = [{ name: 'vod' }]
 
   async run() {
-    const warn = color.yellow;
-    const green = color.green;
-    const red = color.red;
-    const { args, flags } = this.parse(Info);
+    const warn = color.yellow
+    const green = color.green
+    const red = color.red
+    const { args, flags } = this.parse(Info)
     const tokenObj = {
-      client_id: "bc0jezu5plohhw08pud3z5bpojodr9"
-    };
-    const headers = { "Client-ID": tokenObj.client_id };
+      client_id: process.env.TWITCH_CLIENT
+    }
+    const headers = { 'Client-ID': tokenObj.client_id }
     if (args.vod) {
-      const url = args.vod;
-      cli.action.start("Fetching information", "running", { stdout: true });
+      const url = args.vod
+      cli.action.start('Fetching information', 'running', { stdout: true })
       interface InfoObj {
-        name: string;
-        title: string;
-        game: string;
-        length: any;
-        created_at: any;
-        views: any;
-        language: string;
-        resolutions: any;
+        name: string
+        title: string
+        game: string
+        length: any
+        created_at: any
+        views: any
+        language: string
+        resolutions: any
       }
       axios
         .get(
           `https://api.twitch.tv/kraken/videos/${
-            url.indexOf("/") !== -1 ? url.substr(url.lastIndexOf("/") + 1) : url
+            url.indexOf('/') !== -1 ? url.substr(url.lastIndexOf('/') + 1) : url
           }`,
           { headers }
         )
         .then(res => {
-          cli.action.stop(green("complete!"));
+          cli.action.stop(green('complete!'))
           let infoObj: InfoObj = {
             name: res.data.channel.name,
             title: res.data.title,
             game: res.data.game,
             length: moment(res.data.length * 1000 - 60 * 60 * 1000).format(
-              "HH:mm:ss"
+              'HH:mm:ss'
             ),
-            created_at: moment(res.data.created_at).format("DD/MM/YYYY"),
+            created_at: moment(res.data.created_at).format('DD/MM/YYYY'),
             views: res.data.views,
             language: res.data.language,
             resolutions: res.data.resolutions
-          };
-          let resArray = [];
-          for (let elem of Object.keys(infoObj.resolutions)) {
-            let resol = infoObj.resolutions[elem];
-            resArray.push(elem);
           }
-          this.log("-".repeat(80));
-          this.log(`Streamer: ${infoObj.name}`);
-          this.log(`Title: ${infoObj.title}`);
-          this.log(`Game: ${infoObj.game}`);
-          this.log(`Video length: ${infoObj.length}`);
-          this.log(`Video date: ${infoObj.created_at}`);
-          this.log(`Video views: ${infoObj.views}`);
-          this.log(`Language: ${infoObj.language}`);
-          this.log(`Resolutions: ${resArray}`);
+          let resArray = []
+          for (let elem of Object.keys(infoObj.resolutions)) {
+            let resol = infoObj.resolutions[elem]
+            resArray.push(elem)
+          }
+          this.log('-'.repeat(80))
+          this.log(`Streamer: ${infoObj.name}`)
+          this.log(`Title: ${infoObj.title}`)
+          this.log(`Game: ${infoObj.game}`)
+          this.log(`Video length: ${infoObj.length}`)
+          this.log(`Video date: ${infoObj.created_at}`)
+          this.log(`Video views: ${infoObj.views}`)
+          this.log(`Language: ${infoObj.language}`)
+          this.log(`Resolutions: ${resArray}`)
         })
-        .catch(e => this.log(red(e)));
+        .catch(e => this.log(red(e)))
     } else {
-      console.log(red("Error: No url or video id provided"));
-      console.log(warn("Run the command with --help flag to learn more"));
+      console.log(red('Error: No url or video id provided'))
+      console.log(warn('Run the command with --help flag to learn more'))
     }
   }
 }
